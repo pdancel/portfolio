0990979b852dcb55a0ec16a348edc2d0c48ffb88
Patrick Dancel <31138487+pdancel@users.noreply.github.com>
2018-01-29 20:31:46 -0800

    Add files via upload

--- SQL/SQL Queries_PatrickDancel.sql
+++ SQL/SQL Queries_PatrickDancel.sql
@@ -2,49 +2,45 @@
 
 use sakila;
 
--- 1a.
+-- 1a. Display the first and last names of all actors from the table `actor`. 
 select first_name, last_name 
 from actor;
 
--- 1b.
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
 select concat(first_name,' ', last_name) as 'Actor Name'
 from actor;
 
 
--- 2a.
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 select actor_id,first_name, last_name 
 from actor
 where first_name = 'Joe';
 
--- 2b. 
+-- 2b. Find all actors whose last name contain the letters `GEN`
 select last_name 
 from actor
 where last_name like '%GEN%';
 
--- 2c. 
+-- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
 select last_name, first_name
 from actor
 where last_name like '%LI%';
 
--- 2d.
-
+-- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
 select country_id, country
 from country
 where country in ('Afghanistan', 'Bangladesh', 'China');
 
 
--- 3a. 
-	
+-- 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
 ALTER table actor
     add column middle_name varchar(45) AFTER first_name;
   	
--- 3b. 
-
+-- 3b. You realize that some of these actors have tremendously long last names. Change the data type of the `middle_name` column to `blobs`.
 ALTER TABLE actor
 MODIFY COLUMN middle_name blob;
 
--- 3c. 
-
+-- 3c. Now delete the `middle_name` column.
 ALTER TABLE actor DROP COLUMN middle_name;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
